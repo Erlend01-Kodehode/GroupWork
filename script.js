7b8b08157a5ec053bd4f88826ffb36b5b36cd503
Erlend01-Kodehode <erlend01.kodehode@gmail.com>
2024-12-11 12:47:35 +0100

    wish into array functional

--- script.js
+++ script.js
@@ -10,27 +10,50 @@ addWishButton.addEventListener("click", () => {
 // Modal window where you input wishes
 function showSubmitWindow() {
   const modal = document.createElement("dialog");
+  const inputForm = document.createElement("form");
   const inputField = document.createElement("input");
   const submitInput = document.createElement("button");
   const cancelInput = document.createElement("button");
 
   // Classes
   modal.classList.add("DialogueWindow");
+  inputForm.classList.add("FormClass");
   inputField.classList.add("InputClass");
   submitInput.classList.add("Button");
   cancelInput.classList.add("Button");
 
   inputField.placeholder = "Your Wish";
+  inputField.name = "InputField";
   submitInput.textContent = "Submit";
   cancelInput.textContent = "Cancel";
 
   // HTML Structure
-  modal.append(inputField, submitInput, cancelInput);
+  inputForm.append(inputField, submitInput);
+  modal.append(inputForm, cancelInput);
   document.body.append(modal);
 
   // Render Modal
   modal.showModal();
-  // Close Window
+
+  // Submit Form
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(inputForm); // Save Form Data
+    // Error if empty
+    if (!formData.get("InputField")) {
+      console.error("The wish input field is empty.");
+      return;
+    }
+    // Push to array
+    wishes.push({
+      description: formData.get("InputField"),
+    });
+    // Close Modal
+    modal.close();
+    inputForm.removeEventListener("submit", arguments.callee);
+    console.log(wishes);
+  });
+  // Cancel and close Modal
   cancelInput.addEventListener("click", () => {
     modal.close();
     cancelInput.removeEventListener("click", arguments.callee);
@@ -39,3 +62,9 @@ function showSubmitWindow() {
 
 // Force Event
 // showSubmitWindow();
+
+// Save Data to Local Storage
+function saveStateToLocalStorage() {
+  // Serialize wishes arr to JSON before storing to local storage
+  localStorage.setItem("Wishes", JSON.stringify(wishes));
+}
